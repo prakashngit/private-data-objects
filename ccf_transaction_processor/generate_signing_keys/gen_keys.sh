fba19f7c0d1209b627029b590e63b3521b929f27
prakashngit <prakash.narayana.moorthy@intel.com>
2020-04-27 12:37:26 -0700

    moved CCF to tag 0.9.2. Improved Build and Install documentation for PDO TP
    Signed-off-by: prakashngit <prakash.narayana.moorthy@intel.com>

---
+++ ccf_transaction_processor/generate_signing_keys/gen_keys.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Usage ./gen_keys.sh <ip-address-of-CCF>
+
+# Copy KEYs for test
+cp ../CCF/build/workspace/pdo_tp_common/user1_privk.pem .
+cp ../CCF/build/workspace/pdo_tp_common/user1_cert.pem .
+cp ../CCF/build/workspace/pdo_tp_common/networkcert.pem .
+
+# Copy the infra folder
+cp -r ../CCF/tests/infra infra
+
+# activate the env
+source ../CCF/build/env/bin/activate
+
+echo "start ping test : 100 pings"
+python key_gen.py --host $1
+
+
+
---
+++ ccf_transaction_processor/generate_signing_keys/key_read.py
@@ -0,0 +1,46 @@
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from infra.clients import CCFClient
+import argparse
+
+def run(args):
+
+    host = args.host
+    port = 6006
+    cert = "./user1_cert.pem"
+    key = "./user1_privk.pem"
+    cafile="./networkcert.pem"
+    format = "json"
+
+    client = CCFClient(host, port, cert=cert, key=key, ca = cafile, format=format, prefix="users", description="none", \
+        version="2.0",connection_timeout=3, request_timeout=3)
+
+    r=client.rpc("get_ledger_verifying_key", dict())
+    if r.result:
+       print(r.result)
+    else:
+       print(r.error['message'])
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+            "--host",
+            help="IP address of the CCF service",
+            type=str)
+
+    args = parser.parse_args()
+    run(args)
